b6d2201c5c05c2f2b4ef7283768e1e0c67e58532
matts <mattsheldon914@gmail.com>
2025-12-07 15:53:16 +0000

    made it a bit more parsimonious

--- day7/day7.py
+++ day7/day7.py
@@ -15,33 +15,26 @@ with open(INPUT_FILE, "r") as f:
     for row_no, row in enumerate(f):
         row = row.strip()
         new_hb = [False for i in range(N_COLS)]
-        splits = set()
+        new_timelines = [0 for i in range(N_COLS)]
+
         for i, c in enumerate(row):
             if c == "S":
                 new_hb[i] = True
-                timelines[i] = 1
+                new_timelines[i] = 1
             elif has_beam[i]:
                 if c == "^":
                     if i > 0:
                         new_hb[i-1] = True
+                        new_timelines[i-1] += timelines[i]
                     if i < N_COLS-1:
                         new_hb[i+1] = True
+                        new_timelines[i+1] += timelines[i]
                     n_splits += 1
-                    splits.add(i)
                 else:
                     new_hb[i] = True
+                    new_timelines[i] += timelines[i]
+
         has_beam = new_hb
-
-        new_timelines = [0 for i in range(N_COLS)]
-
-        for i, x in enumerate(timelines):
-            if i in splits:
-                if i + 1 < N_COLS:
-                    new_timelines[i+1] += x
-                if i-1 >= 0:
-                    new_timelines[i-1] += x
-            else:
-                new_timelines[i] += x
         timelines = new_timelines
 
 print(f"(Part 1) Number of splits: {n_splits}")
